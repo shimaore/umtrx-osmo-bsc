af3418a7ce4b5699dde18d52ec669c554fd74298
Stephane Alnet <stephane@shimaore.net>
2013-12-28 18:10:58 +0100

    Updated with usage of the `jolly/testing` branch (following on-list discussion).

--- start.sh
+++ start.sh
@@ -10,6 +10,11 @@
 #
 # osmo-nitb -c etc/nitb.cfg -l var/hlr.sqlite3 -T -P -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM:DMNCC:DPAG:DMEAS:DSCCP:DMSC:DMGCP:DHO:DDB:DREF:DGPRS:DNS:DBSSGP:DLLC:DSNDCP:DNAT:DCTRL:DSMPP -a
 
+# Actually, since the Debian packages are currently not built when using
+# the `jolly/testing` branch, you have to start it this way:
+./openbsc/openbsc/src/osmo-nitb/osmo-nitb -c etc/nitb.cfg -l var/hlr.sqlite3 -T -P -C -a \
+  --debug=DRLL:DCC:DMM:DRR:DRSL:DNM:DMNCC:DPAG:DMEAS:DSCCP:DMSC:DMGCP:DHO:DDB:DREF:DGPRS:DNS:DBSSGP:DLLC:DSNDCP:DNAT:DCTRL:DSMPP
+
 # * BSC
 #   This is the one I use for development of a SIP MSC.
 #   Probably not very useful unless you're a carrier and already have an MSC. :)
